438cfd19986b53df395ca0f53fd9bd69ee52b309
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-09-20 20:12:14 -0300

    prashe-v2

--- data/dataPhrases.js
+++ data/dataPhrases.js
@@ -14,6 +14,36 @@ const dataPhrases = [
         es:"Juan  👋",
         en:"Jhon  👋"
     },
+    {
+        id:4,
+        es:"Bien hecho es mejor que bien dicho",
+        en:"Well done is better than well said"
+    },
+    {
+        id:5,
+        es:"El éxito en la gestión requiere aprender tan rápidamente como el mundo va cambiando",
+        en:"Success in management requires learning as fast as the world is changing"
+    },
+    {
+        id:6,
+        es:"Donde hay amor, hay vida",
+        en:"Where there is love there is life"
+    },
+    {
+        id:7,
+        es:"Cuando eliges la esperanza, todo es posible",
+        en:"Once you choose hope, anything’s possible"
+    },
+    {
+        id:8,
+        es:"Vuelve a intentarlo. Fracasa de nuevo. Fracasa mejor",
+        en:" Try it again. Fail again. Fail better"
+    },
+    {
+        id:3,
+        es:"trata de ser el arcoíris en la nube de otra persona",
+        en:"Try to be a rainbow in someone's cloud"
+    }
 ]
 
 module.exports = dataPhrases;
